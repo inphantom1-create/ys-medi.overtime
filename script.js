25d6510c50acc2bb9c0b904b65298556d56fd6e4
inphantom1-create <inphantom1@gmail.com>
2026-08-28 17:17:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* ============================================================
    시간외 근무 신청 시스템 - script.js
    ============================================================ */
-const GAS_URL        = 'https://script.google.com/macros/s/AKfycbzjD764qLr-6V1L9v7xVmWJw9DSBHQ3AfWldlGHk6p5lfu84fV2n5SgFFIri-dCHsuymw/exec';
+const GAS_URL        = 'https://script.google.com/macros/s/AKfycbydGqCR6HlJAcAs7UmjzSJZtye4eU1dofkkqRrLl6BfQPOST0qQgcGCH9FO-CYCkLs9_g/exec';
 const ADMIN_PASSWORD = '1234';
 const WORK_START  = timeToMin('08:30');
 const WORK_END    = timeToMin('17:30');
